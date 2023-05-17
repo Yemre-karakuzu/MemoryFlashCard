d1157a9fa4ff1e5e94d62503a72f768325a38ea4
Yunus Emre Karakuzu <48347688+Yemre-karakuzu@users.noreply.github.com>
2023-05-17 19:32:54 +0300

    bug fixing

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,18 +1,20 @@
 <template>
   <div @click="flipCardEmit" class="card-content">
-    <div v-if="card.isFlipped">{{ card.value }}</div>
+    <div v-if="cardItem.isFlipped">{{ cardItem.value }}</div>
     <div v-else>?</div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import { ICard } from "../../interface/Card.ts";
+import { ICard } from "../../interface/Card";
 
 @Component
 export default class Card extends Vue {
-  @Prop() cardItem:  Array<ICard>();
-  @Prop() index: number;
+  @Prop()
+cardItem!: Array<ICard>;
+  @Prop()
+index!: number;
   flipCardEmit() {
     this.$emit("flip", this.index);
   }
--- src/components/Card/Cards.vue
+++ src/components/Card/Cards.vue
@@ -2,12 +2,7 @@
   <div class="game-board">
     <div>score: {{ score }}</div>
     <div class="game-board-wrapper">
-      <div
-        v-for="(card, index) in cards"
-        :key="index"
-        class="card"
-        @click="flipCard(index)"
-      >
+      <div v-for="(card, index) in cards" :key="index" class="card" @click="flipCard(index)">
         <Card :cardItem="card" :index="index" @flip="flipCard($event)" />
       </div>
     </div>
@@ -16,69 +11,72 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import Card from "./Card";
-import { ICard } from "../../interface/Card.js";
+import Card from "./Card.vue";
+import { ICard } from "../../interface/Card";
 
 @Component({
   name: "game",
   components: {
     Card,
   },
-  created() {
-    this.cardCount = parseInt(localStorage.getItem("level"));
-    this.randomDiziOlustur();
-  },
+  // created() {
+  //   this.cardCount = parseInt(localStorage.getItem("level"));
+  //   this.randomDiziOlustur();
+  // }  
 })
 export default class Cards extends Vue {
-    cardCount= 0 as number,
-    cards= [] as Array<ICard>(),
-    flippedCards= [] as Array<ICard>(),
-    score= "0" as string,
-    
-    randomDiziOlustur() : void {
-      // 1'den cardCount/2'e kadar olan sayıları 2 kez diziye ekliyoruz
-      for (let i = 1; i <= this.cardCount / 2; i++) {
-        this.cards.push({ value: i, isMatch: false, isFlipped: false });
-        this.cards.push({ value: i, isMatch: false, isFlipped: false });
-      }
-      // Diziyi karıştırıyoruz
-      for (let i = this.cards.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = this.cards[i];
-        this.cards[i] = this.cards[j];
-        this.cards[j] = temp;
-      }
-    },
-    flipCard(index: number) {
-      const card: ICard = this.cards[index];
+  cardCount = 0 as number;
+  cards = [] as Array<ICard>;
+  flippedCards = [] as Array<ICard>;
+  score = "0" as string;
+  created() {
+    this.cardCount = parseInt(localStorage.getItem('level') || '');
+    this.randomDiziOlustur();
+  }
+  randomDiziOlustur(): void {
+    // 1'den cardCount/2'e kadar olan sayıları 2 kez diziye ekliyoruz
+    for (let i = 1; i <= this.cardCount / 2; i++) {
+      this.cards.push({ value: i, isMatch: false, isFlipped: false });
+      this.cards.push({ value: i, isMatch: false, isFlipped: false });
+    }
+    // Diziyi karıştırıyoruz
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+    }
+  }
+  flipCard(index: number) {
+    const card: ICard = this.cards[index];
 
-      // Zaten döndürülmüş kartı tekrar döndürmeyi engelle
-      if (card.isFlipped || card.isMatch) {
-        return;
-      }
+    // Zaten döndürülmüş kartı tekrar döndürmeyi engelle
+    if (card.isFlipped || card.isMatch) {
+      return;
+    }
 
-      card.isFlipped = true;
-      this.flippedCards.push(card);
+    card.isFlipped = true;
+    this.flippedCards.push(card);
 
-      if (this.flippedCards.length === 2) {
-        // İki kart da döndürüldü, eşleşme kontrolü yap
-        setTimeout(() => {
-          const [card1, card2] = this.flippedCards;
-          if (card1.value === card2.value) {
-            card1.isMatch = true;
-            card2.isMatch = true;
-            this.score =(parseInt(this.score)+10).toString();
-            localStorage.setItem("score", this.score);
-          } else {
-            this.score =(parseInt(this.score)-1).toString();
-            localStorage.setItem("score", this.score);
-            card1.isFlipped = false;
-            card2.isFlipped = false;
-          }
-          this.flippedCards = [];
-        }, 500);
-      }
-    },
+    if (this.flippedCards.length === 2) {
+      // İki kart da döndürüldü, eşleşme kontrolü yap
+      setTimeout(() => {
+        const [card1, card2] = this.flippedCards;
+        if (card1.value === card2.value) {
+          card1.isMatch = true;
+          card2.isMatch = true;
+          this.score = (parseInt(this.score) + 10).toString();
+          localStorage.setItem("score", this.score);
+        } else {
+          this.score = (parseInt(this.score) - 1).toString();
+          localStorage.setItem("score", this.score);
+          card1.isFlipped = false;
+          card2.isFlipped = false;
+        }
+        this.flippedCards = [];
+      }, 500);
+    }
+  }
 }
 </script>
 <style>
@@ -89,17 +87,20 @@ export default class Cards extends Vue {
   height: 100%;
   flex-direction: column;
 }
+
 .game-board-wrapper {
   display: flex;
   flex-wrap: wrap;
   width: 320px;
 }
+
 .btn-group {
   display: flex;
   width: 20%;
   justify-content: space-between;
   align-items: center;
 }
+
 .card {
   width: 60px;
   height: 60px;
